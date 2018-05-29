00774521036e4eb7f98bccb56a32c6ff96c1e2b6
Васильева <@223_16>
2018-05-29 21:17:20 +0600

    Add project files.

---
+++ Ftp.lib/ServiceFtp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ftp
+{
+    public class ServiceFtp
+    {
+        private NetworkCredential credential;
+        private string path;
+
+        public ServiceFtp(NetworkCredential credential, string path)
+        {
+            this.credential = credential;
+            this.path = path;
+        }
+        public void DownloadFile()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(path); // запрос
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+
+            request.Credentials = credential;
+            request.Proxy = null;
+
+            var responce = (FtpWebResponse)request.GetResponse();
+            Stream stream = responce.GetResponseStream();
+
+            if (stream != null)
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var line = reader.ReadLine();
+                    while (!string.IsNullOrEmpty(line))
+                    {
+                        Console.WriteLine(line);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Ftp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ftp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceFtp sftp = new ServiceFtp(
+                new NetworkCredential("AvisSite", "dm#o4%v#Wn43arWv2"), "ftp://new.avislogistics.kz:2021/GOODS_RECEIPT/");
+
+            sftp.DownloadFile();
+        }
+    }
+}
